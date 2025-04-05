d190d6b28c20c097e94ebc55c5a1912adbfbd455
Seunoluwa1991 <kilashomichael@gmail.com>
2025-04-05 19:11:17 +0100

    second-steamlit-project-commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function multiply(num1, num2){
+    let answer= num1 * num2;
+    return answer;
+}
+console.log(multiply(5,2))
+console.log(multiply(5,3))
+console.log(multiply(5,4))
+
+document.querySelector("html").addEventListener('click', ()=>{
+    alert('This is an event creating an interactivity in the html')
+});
+
+const button = document.querySelector('#button');
+
+// button.addEventListener('click', ()=>{
+
+// })
